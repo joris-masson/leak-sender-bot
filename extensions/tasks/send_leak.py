74fdb0c05142d96af00bc81efd0cda07c2b7f4bd
Joris Masson <68601166+joris-masson@users.noreply.github.com>
2026-02-09 23:16:53 +0100

    Rajout d'un bouton pour marquer un leak comme spoil, il sera supprimé et redirigé vers un fil lié au salon d'origine

--- extensions/tasks/send_leak.py
+++ extensions/tasks/send_leak.py
@@ -1,4 +1,5 @@
-from interactions import Task, IntervalTrigger, Extension, listen, GuildText
+from interactions import Task, IntervalTrigger, Extension, listen, GuildText, Button, ButtonStyle, GuildPublicThread
+from interactions.api.events import Component
 
 from leak import Leak
 from utils import open_db_connection
@@ -9,13 +10,29 @@ class SendLeak(Extension):
     async def on_startup(self):
         self.send_new_leaks.start()
 
-        self.GAMES = [
-            "GI",
-            "HSR",
-            "ZZZ"
-        ]
-
         self.BOT_GUILDS = {str(guild.id) for guild in self.bot.guilds}
+
+        await self.send_new_leaks()
+
+    @listen(Component)
+    async def on_component(self, event: Component):
+        ctx = event.ctx
+
+        leak_id = ctx.custom_id.replace("button_spoil_", '')
+        leak = self.get_leak(leak_id)
+        message_id = self.get_leak_message_id(leak_id, str(ctx.guild.id), str(ctx.channel.id))
+
+        original_leak_message = self.bot.get_channel(ctx.channel.id).get_message(message_id)
+
+        threads = await ctx.channel.fetch_all_threads()
+        for thread in threads.threads:
+            if thread.name == "Spoil":
+                await original_leak_message.delete()
+                await self.send_leak(leak, thread, False)
+                return
+        thread = await ctx.channel.create_public_thread("Spoil")
+        await original_leak_message.delete()
+        await self.send_leak(leak, thread, False)
 
     @Task.create(IntervalTrigger(minutes=15))
     async def send_new_leaks(self):
@@ -26,7 +43,6 @@ class SendLeak(Extension):
             for guild_id, leak in new_leaks[channel_id]:
                 if guild_id in self.BOT_GUILDS:
                     await self.send_leak(leak, channel)
-
 
     def get_new_leaks(self) -> dict[tuple[str, set[Leak]]]:
         db = open_db_connection()
@@ -73,14 +89,65 @@ class SendLeak(Extension):
 
         return res
 
-    async def send_leak(self, leak: Leak, channel: GuildText):
+    def get_leak(self, leak_id: str) -> Leak:
+        db = open_db_connection()
+        cursor = db.cursor()
+
+        query = f"SELECT id, timestamp, game, title, link, author_name, media_link FROM Kazooha.Leak WHERE id='{leak_id}'"
+        cursor.execute(query)
+
+        leak = cursor.fetchone()
+
+        cursor.close()
+        db.close()
+
+        return Leak(
+            leak[0],
+            leak[1],
+            leak[2],
+            leak[3],
+            leak[4],
+            leak[5],
+            leak[6]
+        )
+
+    def get_leak_message_id(self, leak_id: str, guild_id: str, channel_id: str) -> str:
+        db = open_db_connection()
+        cursor = db.cursor()
+
+        query = f"SELECT message_id FROM Kazooha.LeakMessage WHERE leak_id=%s AND guild_id=%s AND channel_id=%s"
+        cursor.execute(query, (
+            leak_id,
+            guild_id,
+            channel_id
+        ))
+
+        message_id = cursor.fetchone()
+
+        cursor.close()
+        db.close()
+
+        return message_id[0]
+
+    async def send_leak(self, leak: Leak, channel: GuildText | GuildPublicThread, show_spoil_button: bool = True):
         leak_embed = leak.get_embed()
 
         db = open_db_connection()
         cursor = db.cursor()
 
         query = "INSERT INTO Kazooha.LeakMessage (leak_id, guild_id, channel_id, message_id) VALUES (%s, %s, %s, %s)"
-        message = await channel.send(embeds=leak_embed)
+
+        if show_spoil_button:
+            spoil_button = Button(
+                custom_id=f"button_spoil_{leak.leak_id}",
+                style=ButtonStyle.GRAY,
+                label="Spoil !",
+            )
+
+            message = await channel.send(embeds=leak_embed, components=spoil_button)
+        else:
+            message = await channel.send(embeds=leak_embed)
+
         cursor.execute(query, (
             leak.leak_id,
             str(channel.guild.id),
